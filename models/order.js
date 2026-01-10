a91d3b208bae325aa9a36c8cf0b0c026c41dbcdb
mr-mashanlo <mr.mashanlo@yandex.ru>
2026-01-10 17:05:34 +0500

    Refactor order model

--- models/order.js
+++ models/order.js
@@ -4,7 +4,7 @@ import z from 'zod';
 
 export const OrderDTO = z.object( {
   address: z.string(),
-  products: z.array( z.object( { product: z.string(), price: z.number(), quantity: z.number() } ) ),
+  products: z.array( z.object( { _id: z.string(), price: z.number(), quantity: z.number() } ) ),
   totalPrice: z.number(),
   totalQuantity: z.number()
 } );
@@ -13,21 +13,21 @@ export const Order = z.object( {
   _id: z.string(),
   user: z.string(),
   address: z.string(),
-  products: z.array( z.object( { product: z.string(), price: z.number(), quantity: z.number() } ) ),
+  products: z.array( z.object( { _id: z.string(), price: z.number(), quantity: z.number() } ) ),
   status: z.enum( [ 'Processing', 'Shipped', 'Delivered', 'Canceled' ] ),
   totalPrice: z.number(),
   totalQuantity: z.number(),
-  createdAt: z.number()
+  createdAt: z.string()
 } );
 
 export const OrderSchema = new Schema( {
   user: { type: Schema.Types.ObjectId, ref: 'User', require: true, index: true },
   address: { type: Schema.Types.ObjectId, ref: 'Address', require: true, index: true, autopopulate: true },
-  products: [ { product: { type: Schema.Types.ObjectId, ref: 'Product', require: true }, price: { type: Number, default: 0 }, quantity: { type: Number, default: 0 } } ],
+  products: [ { _id: { type: Schema.Types.ObjectId, ref: 'Product', require: true }, price: { type: Number, default: 0 }, quantity: { type: Number, default: 0 } } ],
   status: { type: String, default: 'Processing' },
   totalPrice: { type: Number, default: 0 },
   totalQuantity: { type: Number, default: 0 },
-  createdAt: { type: Number }
+  createdAt: { type: String }
 } );
 
 OrderSchema.plugin( autopopulate );
